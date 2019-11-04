525cc53c5e90cb011d50587741ebc944c54525de
unknown <r1m1nrao@PUNB-TR04-06.hexa.training.local>
2019-11-04 17:27:31 +0530

    Docker static webapp

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SampleWebApp.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace SampleWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        IConfiguration configuration;
+
+        public HomeController(IConfiguration configuration )
+        {
+          this.configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+            ViewBag.Name = configuration.GetValue<string>("Name");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
